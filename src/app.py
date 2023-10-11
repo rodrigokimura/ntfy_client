0c0ef34fa1d5bf969bb658c1482762e0b29a3ef4
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2023-10-11 00:56:32 -0300

    First commit

---
+++ src/app.py
@@ -0,0 +1,33 @@
+import requests
+
+from config import SERVER, TOKEN, TOPIC
+
+
+class Ntfy:
+    def __init__(self, server: str, topic: str, token: str):
+        self.server = server
+        self.topic = topic
+        self.token = token
+
+    def send(
+        self,
+        message: str,
+        title: str = "",
+        priority: str = "urgent",
+    ):
+        response = requests.post(
+            url=f"{self.server}/{self.topic}",
+            data=message,
+            headers={
+                "Authorization": f"Bearer {self.token}",
+                "Title": title or message,
+                "Priority": priority,
+                "Tags": "warning",
+            },
+        )
+        print(response.status_code)
+
+
+if __name__ == "__main__":
+    app = Ntfy(SERVER, TOPIC, TOKEN)
+    app.send("test")
---
+++ src/config.py
@@ -0,0 +1,5 @@
+from decouple import config
+
+SERVER = config("SERVER")
+TOPIC = config("TOPIC")
+TOKEN = config("TOKEN")
